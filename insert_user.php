01567e0dd0d5b36976120d5878e6b646228e4b22
Maria Eduarda <e107ti@etec.sp.gov.br>
2024-04-08 20:50:11 -0300

    html concluído. php e conexão com banco de dados iniciada

---
+++ insert_user.php
@@ -0,0 +1,18 @@
+<?php
+//recebendo dados do formulário
+
+$nome = $_POST['nome'];
+$setor = $_POST['setor'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+//exibe o nome que está armazenado na variavel $nome
+echo "$nome";
+echo "<br>";
+
+//inserindo dados no banco
+
+
+
+
+?>
